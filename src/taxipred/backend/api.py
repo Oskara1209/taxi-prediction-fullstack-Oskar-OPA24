882bfb88ef71c068cddf8a0eb2e6cf93fc6bab04
Oskara1209 <oskar.akerman12@outlook.com>
2025-09-30 18:48:11 +0200

    imported clean data

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from taxipred.backend.data_processing import TaxiData
+from taxipred.backend.data_processing import CleanTaxiData
 
 app = FastAPI()
 
-taxi_data = TaxiData()
+taxi_data = CleanTaxiData()
 
 @app.get("/taxi/")
 async def read_taxi_data():
