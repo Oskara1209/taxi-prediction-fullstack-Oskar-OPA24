61108bfbd730969725bb03585d4d843d1ce5d205
Oskara1209 <oskar.akerman12@outlook.com>
2025-10-02 17:48:14 +0200

    Load API key

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 from taxipred.backend.data_processing import CleanTaxiData
+from dotenv import load_dotenv 
+import os 
+import requests
+
+load_dotenv()
 
 app = FastAPI()
+
+API_KEY = os.getenv("GRAPHOPPER_API_KEY") 
+if not API_KEY:
+    raise RuntimeError("GRAPHOPPER_API_KEY saknas")
+
+BASE = "https://graphhopper.com/api/1"
+TIMEOUT = 15
 
 taxi_data = CleanTaxiData()
 
