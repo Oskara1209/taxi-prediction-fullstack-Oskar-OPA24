b21855386f80cb51abbd8d9027307da728a09b5b
Oskara1209 <oskar.akerman12@outlook.com>
2025-10-08 16:43:18 +0200

    added model_path

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -4,4 +4,6 @@ TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
 
 CLEAN_TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing_clean.csv")
 
+MODELS_PATH = files("taxipred").joinpath("backend/models/taxi_model.joblib")
+
 # DATA_PATH = Path(__file__).parents[1] / "data"
